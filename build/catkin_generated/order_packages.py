64d251656bedde42f6b5bb94f3c8c21e6a265d2f
zhuhao425 <zh413678271@163.com>
2019-10-14 21:24:58 +0800

    delete some useless files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/zh/workspace/multi_lidar_scan/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/zh/workspace/multi_lidar_scan/devel;/opt/ros/kinetic".split(';') if "/home/zh/workspace/multi_lidar_scan/devel;/opt/ros/kinetic" != "" else []
--- build/turtlebot_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zh/workspace/multi_lidar_scan/src/turtlebot_teleop
-# Build directory: /home/zh/workspace/multi_lidar_scan/build/turtlebot_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
