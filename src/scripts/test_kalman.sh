658dc992d023eeea08782fcfb5053db2d2bde3b1
zhuhao425 <zh413678271@163.com>
2020-03-15 19:32:56 +0800

    algorithm

--- src/scripts/test_kalman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm -e "source ../../devel/setup.bash; roslaunch myrobot turtlebot_world.launch;read" &
-sleep 5
-xterm -e "source ../../devel/setup.bash; roslaunch rf2o_laser_odometry rf2o_laser_odometry.launch;read" &
-sleep 5
-xterm -e "source ../../devel/setup.bash; roslaunch robot_pose_ekf robot_pose_ekf.launch;read" &
-sleep 5
-xterm -e "source ../../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch;read"
